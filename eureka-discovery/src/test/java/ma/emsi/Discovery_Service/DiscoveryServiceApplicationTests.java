50f3406feb04c002097032073d240a4eb2c4dbe7
kabbach55 <kabbach_zakaria@emsi-edu.ma>
2021-12-26 13:23:19 +0100

    adding eureka-service

--- eureka-discovery/src/test/java/ma/emsi/Discovery_Service/DiscoveryServiceApplicationTests.java
+++ eureka-discovery/src/test/java/ma/emsi/Discovery_Service/DiscoveryServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package ma.emsi.eurekadiscovery;
+package ma.emsi.Discovery_Service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaDiscoveryApplicationTests {
+class DiscoveryServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
